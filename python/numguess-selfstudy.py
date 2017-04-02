a6acb4ea513f60e46858aea0b35dc01f9396a313
juliahwang <qufskan9396@gmail.com>
2017-04-02 21:12:48 +0900

    input numguess-selfstudy.py: print user's name and put number

--- python/numguess-selfstudy.py
+++ python/numguess-selfstudy.py
@@ -3,3 +3,7 @@ import random
 
 answer = random.randint(1, 100)
 print(answer)
+
+username = input("What is your name? ")
+guess = eval(input("Hi, " + username + ". Guess the number: "))
+print(guess)
